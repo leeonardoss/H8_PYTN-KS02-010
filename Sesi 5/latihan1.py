9007a994813a7023516b516fbbc1ad5a84396445
leeonardoss <85247620+leeonardoss@users.noreply.github.com>
2022-04-10 21:29:20 +0700

    Delete Sesi 5 directory

--- Sesi 5/latihan1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("https://raw.githubusercontent.com/ardhiraka/PFDS_sources/master/nbaallelo.csv")
-print(len(df))
-print(df.shape)
-print(df.head())
