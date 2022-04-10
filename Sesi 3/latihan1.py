eb371cb79fbeabd88fa4ce01991cd2d84fa7f5e4
leeonardoss <85247620+leeonardoss@users.noreply.github.com>
2022-04-10 21:29:05 +0700

    Delete Sesi 3 directory

--- Sesi 3/latihan1.py
+++
@@ -1,9 +0,0 @@
-p = 5
-l = 10
-keliling_s4 = 2*(p+l)
-luas_s4 = p*l
-print("panjang segi empat = ", p)
-print("lebar segi empat = ", l)
-print("keliling segi empat = ", keliling_s4)
-print("luas segi empat = ", luas_s4)
-
--- Sesi 3/latihan3.py
+++
@@ -1,7 +0,0 @@
-alas = input("Masukkan nilai alas: ")
-tinggi = input("Masukkan nilai tinggi: ")
-luas = int(alas)*int(tinggi)/2
-print("alas segitiga sebesar ", alas)
-print("tinggi segitiga sebesar ", tinggi)
-print("luas segitiga seluas ", luas)
-
